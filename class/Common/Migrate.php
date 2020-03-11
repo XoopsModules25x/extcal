5c862154932258469c315d0574f69e706285b756
mambax7 <mambax7@gmail.com>
2020-03-11 08:40:48 -0400

    added renameColumns in Migration

--- class/Common/Migrate.php
+++ class/Common/Migrate.php
@@ -18,24 +18,29 @@ namespace XoopsModules\Extcal\Common;
  * @category  Migrate
  * @author    Richard Griffith <richard@geekwright.com>
  * @copyright 2016 XOOPS Project (https://xoops.org)
- * @license   GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @license   GNU GPL 2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
  * @link      https://xoops.org
  */
+//use XoopsModules\Extcal\Common;
+
 class Migrate extends \Xmf\Database\Migrate
 {
     private $renameTables;
 
     /**
      * Migrate constructor.
-     * @param \XoopsModules\Extcal\Common\Configurator $configurator
+     * @param Configurator $configurator
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
      */
-    public function __construct(\XoopsModules\Extcal\Common\Configurator $configurator)
+    public function __construct(Configurator $configurator = null)
     {
-        //   require_once  dirname(dirname(__DIR__)) . '/include/config.php';
-        $this->renameTables = $configurator->renameTables;
+        if (null !== $configurator) {
+            $this->renameTables = $configurator->renameTables;
 
-        $moduleDirName = basename(dirname(dirname(__DIR__)));
-        parent::__construct($moduleDirName);
+            $moduleDirName = basename(dirname(dirname(__DIR__)));
+            parent::__construct($moduleDirName);
+        }
     }
 
     /**
@@ -44,39 +49,20 @@ class Migrate extends \Xmf\Database\Migrate
     private function changePrefix()
     {
         foreach ($this->renameTables as $oldName => $newName) {
-            if ($this->tableHandler->useTable($oldName)) {
+            if ($this->tableHandler->useTable($oldName) && !$this->tableHandler->useTable($newName)) {
                 $this->tableHandler->renameTable($oldName, $newName);
             }
         }
     }
 
-    /**
-     * Change integer IPv4 column to varchar IPv6 capable
-     *
-     * @param string $tableName  table to convert
-     * @param string $columnName column with IP address
-     */
-    private function convertIPAddresses($tableName, $columnName)
+    private function renameColumns($tableName, $columnName, $newName)
     {
         if ($this->tableHandler->useTable($tableName)) {
             $attributes = $this->tableHandler->getColumnAttributes($tableName, $columnName);
-            if (false !== mb_strpos($attributes, ' int(')) {
-                if (false === mb_strpos($attributes, 'unsigned')) {
-                    $this->tableHandler->alterColumn($tableName, $columnName, " bigint(16) NOT NULL  DEFAULT '0' ");
-                    $this->tableHandler->update($tableName, [$columnName => "4294967296 + $columnName"], "WHERE $columnName < 0", false);
-                }
-                $this->tableHandler->alterColumn($tableName, $columnName, " varchar(45)  NOT NULL  DEFAULT '' ");
-                $this->tableHandler->update($tableName, [$columnName => "INET_NTOA($columnName)"], '', false);
+            if (false !== strpos($attributes, ' int(')) {
+                $this->tableHandler->alterColumn($tableName, $columnName, $attributes, $newName);
             }
         }
-    }
-
-    /**
-     * Move do* columns from tdmmoney_posts to tdmmoney_posts_text table
-     */
-    private function moveDoColumns()
-    {
-        //for an example, see newbb 5.0
     }
 
     /**
@@ -92,10 +78,7 @@ class Migrate extends \Xmf\Database\Migrate
         if ($this->renameTables && is_array($this->renameTables)) {
             $this->changePrefix();
         }
-        //        // columns dohtml, dosmiley, doxcode, doimage and dobr moved between tables as some point
-        //        $this->moveDoColumns();
-        //        // Convert IP address columns from int to readable varchar(45) for IPv6
-        //        $this->convertIPAddresses('extgallery_posts', 'poster_ip');
-        //        $this->convertIPAddresses('extgallery_report', 'reporter_ip');
+        $this->renameColumns('extcal_event', 'event_etablissement', 'event_location');
     }
+
 }
