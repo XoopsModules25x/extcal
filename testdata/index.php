e42c647c9e524f71c8f1526a454a267f7f2de14a
mambax7 <mambax7@gmail.com>
2020-03-11 08:39:03 -0400

    save test data in language folder

--- testdata/index.php
+++ testdata/index.php
@@ -7,8 +7,8 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
- * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @copyright       XOOPS Project (https://xoops.org)
+ * @license         GNU GPL 2 (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
  * @package
  * @since           2.5.9
  * @author          Michael Beck (aka Mamba)
@@ -16,14 +16,32 @@
 
 use XoopsModules\Extcal;
 use XoopsModules\Extcal\Common;
+use XoopsModules\Extcal\Utility;
 
-require dirname(dirname(dirname(__DIR__))) . '/mainfile.php';
-include dirname(__DIR__) . '/preloads/autoloader.php';
+require_once dirname(dirname(dirname(__DIR__))) . '/include/cp_header.php';
+require dirname(__DIR__) . '/preloads/autoloader.php';
+
 $op = \Xmf\Request::getCmd('op', '');
+
+$moduleDirName = basename(dirname(__DIR__));
+$moduleDirNameUpper = mb_strtoupper($moduleDirName);
+
+$helper = Extcal\Helper::getInstance();
+// Load language files
+$helper->loadLanguage('common');
 
 switch ($op) {
     case 'load':
-        loadSampleData();
+        if (\Xmf\Request::hasVar('ok', 'REQUEST') && 1 == $_REQUEST['ok']) {
+            if (!$GLOBALS['xoopsSecurity']->check()) {
+                redirect_header('../admin/index.php', 3, implode(',', $GLOBALS['xoopsSecurity']->getErrors()));
+            }
+            loadSampleData();
+        } else {
+            xoops_cp_header();
+            xoops_confirm(['ok' => 1, 'op' => 'load'], 'index.php', sprintf(constant('CO_' . $moduleDirNameUpper . '_' . 'ADD_SAMPLEDATA_OK')), constant('CO_' . $moduleDirNameUpper . '_' . 'CONFIRM'), true);
+            xoops_cp_footer();
+        }
         break;
     case 'save':
         saveSampleData();
@@ -34,29 +52,28 @@ switch ($op) {
 
 function loadSampleData()
 {
+    global $xoopsConfig;
     $moduleDirName = basename(dirname(__DIR__));
-    $helper        = Extcal\Helper::getInstance();
-    $utility       = new Extcal\Utility();
-    $configurator  = new Common\Configurator();
-    // Load language files
-    $helper->loadLanguage('admin');
-    $helper->loadLanguage('modinfo');
-    $helper->loadLanguage('common');
+    $moduleDirNameUpper = mb_strtoupper($moduleDirName);
 
-    //    $items = \Xmf\Yaml::readWrapped('quotes_data.yml');
-    //    \Xmf\Database\TableLoad::truncateTable($moduleDirName . '_quotes');
-    //    \Xmf\Database\TableLoad::loadTableFromArray($moduleDirName . '_quotes', $items);
+    $utility      = new Extcal\Utility();
+    $configurator = new Common\Configurator();
 
     $tables = \Xmf\Module\Helper::getHelper($moduleDirName)->getModule()->getInfo('tables');
 
+    $language = 'english/';
+    if (is_dir(__DIR__ . '/' . $xoopsConfig['language'])) {
+        $language = $xoopsConfig['language'] . '/';
+        }
+
     foreach ($tables as $table) {
-        $tabledata = \Xmf\Yaml::readWrapped($table . '.yml');
+        $tabledata = \Xmf\Yaml::readWrapped($language . $table . '.yml');
         \Xmf\Database\TableLoad::truncateTable($table);
         \Xmf\Database\TableLoad::loadTableFromArray($table, $tabledata);
     }
 
     //  ---  COPY test folder files ---------------
-    if ($configurator->copyTestFolders && is_array($configurator->copyTestFolders)) {
+    if (is_array($configurator->copyTestFolders) && count($configurator->copyTestFolders) > 0) {
         //        $file = __DIR__ . '/../testdata/images/';
         foreach (array_keys($configurator->copyTestFolders) as $i) {
             $src  = $configurator->copyTestFolders[$i][0];
@@ -64,19 +81,26 @@ function loadSampleData()
             $utility::rcopy($src, $dest);
         }
     }
-
     redirect_header('../admin/index.php', 1, constant('CO_' . $moduleDirNameUpper . '_' . 'SAMPLEDATA_SUCCESS'));
 }
 
 function saveSampleData()
 {
-    $moduleDirName      = basename(dirname(__DIR__));
+    global $xoopsConfig;
+    $moduleDirName = basename(dirname(__DIR__));
     $moduleDirNameUpper = mb_strtoupper($moduleDirName);
 
     $tables = \Xmf\Module\Helper::getHelper($moduleDirName)->getModule()->getInfo('tables');
 
+        $languageFolder = __DIR__ . '/' . $xoopsConfig['language'];
+    if (!file_exists($languageFolder . '/')) {
+       Utility::createFolder($languageFolder . '/');
+    }
+    $exportFolder = $languageFolder . '/Exports-' . date('Y-m-d-H-i-s') . '/';
+    Utility::createFolder($exportFolder);
+
     foreach ($tables as $table) {
-        \Xmf\Database\TableLoad::saveTableToYamlFile($table, $table . '_' . date('Y-m-d H-i-s') . '.yml');
+        \Xmf\Database\TableLoad::saveTableToYamlFile($table, $exportFolder . $table . '.yml');
     }
 
     redirect_header('../admin/index.php', 1, constant('CO_' . $moduleDirNameUpper . '_' . 'SAMPLEDATA_SUCCESS'));
@@ -84,16 +108,18 @@ function saveSampleData()
 
 function exportSchema()
 {
+    $moduleDirName      = basename(dirname(__DIR__));
+    $moduleDirNameUpper = mb_strtoupper($moduleDirName);
+
     try {
-        $moduleDirName      = basename(dirname(__DIR__));
-        $moduleDirNameUpper = mb_strtoupper($moduleDirName);
-
-        $migrate = new  \Xmf\Database\Migrate($moduleDirName);
-        $migrate->saveCurrentSchema();
-
-        redirect_header('../admin/index.php', 1, constant('CO_' . $moduleDirNameUpper . '_' . 'EXPORT_SCHEMA_SUCCESS'));
+        // TODO set exportSchema
+        //        $migrate = new Extcal\Migrate($moduleDirName);
+        //        $migrate->saveCurrentSchema();
+        //
+        //        redirect_header('../admin/index.php', 1, constant('CO_' . $moduleDirNameUpper . '_' . 'EXPORT_SCHEMA_SUCCESS'));
     }
     catch (\Exception $e) {
         exit(constant('CO_' . $moduleDirNameUpper . '_' . 'EXPORT_SCHEMA_ERROR'));
     }
+
 }
