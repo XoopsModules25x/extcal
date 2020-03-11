9285ba83fc38b16f27820e4a3343d0264a40392f
mambax7 <mambax7@gmail.com>
2020-03-11 08:41:05 -0400

    cosmetics

--- class/EventNotMemberHandler.php
+++ class/EventNotMemberHandler.php
@@ -14,7 +14,7 @@ namespace XoopsModules\Extcal;
 
 /**
  * @copyright    {@link https://xoops.org/ XOOPS Project}
- * @license      {@link http://www.gnu.org/licenses/gpl-2.0.html GNU GPL 2 or later}
+ * @license      {@link https://www.gnu.org/licenses/gpl-2.0.html GNU GPL 2 or later}
  * @package      extcal
  * @since
  * @author       XOOPS Development Team,
@@ -22,7 +22,7 @@ namespace XoopsModules\Extcal;
 
 use XoopsModules\Extcal;
 
-// defined('XOOPS_ROOT_PATH') || die('Restricted access');
+// defined('XOOPS_ROOT_PATH') || exit('Restricted access');
 
 // // require_once __DIR__ . '/ExtcalPersistableObjectHandler.php';
 
