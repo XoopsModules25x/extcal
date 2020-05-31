2cb6e08f44d537d60a6a6f9be0fc35438edbe518
mambax7 <mambax7@gmail.com>
2020-05-31 08:42:25 -0400

    'mkdir(...)' race condition

--- versions/Extcal_2_21.php
+++ versions/Extcal_2_21.php
@@ -33,7 +33,9 @@ class Extcal_2_21
         // Create eXtcal upload directory if don't exist
         $dir = XOOPS_ROOT_PATH . '/uploads/extcal';
         if (!is_dir($dir)) {
-            mkdir($dir);
+            if (!mkdir($dir) && !is_dir($dir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+            }
 
             // Copy index.html files on uploads folders
             $indexFile = __DIR__ . '/index.html';
